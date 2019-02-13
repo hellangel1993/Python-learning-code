8911fc4aabea6b5eb87e0d1dba676aee67f64fe8
Himanshu Chowdhary <himanshu.chowdhary@qvantel.com>
2019-02-13 19:49:59 +0530

    nA

---
+++ PythonApplication1/Working with number.py
@@ -0,0 +1,10 @@
+from math import *
+
+print(2);
+print(2.33333);
+print(-2333);
+print(3+4);
+print(3+2.333);
+print(4*2);
+print(15/3);
+print(3*(4+5))
--- PythonApplication1/variable.py
+++ PythonApplication1/variable.py
@@ -1,4 +1,4 @@
 name = "himanshu"
 age= 50
 is_male=True
-print("my name is "+name+"My age is " +age)
+print("my name is "+name+"My age is " +str(age))
